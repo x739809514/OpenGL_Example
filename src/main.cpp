59cad4d77cd5987dc23e8df7d83230c0c03bde10
x739809514 <1612650023@qq.com>
2024-02-16 15:59:20 +0000

    Add MVP matrix

--- src/main.cpp
+++ src/main.cpp
@@ -62,21 +62,20 @@ int main()
     glViewport(0, 0, 800, 600);
 
     float positions[] = {
-        -0.5f,-0.5f,0.0f,0.0f,
-        0.5f,-0.5f,1.0f,0.0f,
-        0.5f,0.5f,1.0f,1.0f,
-        -0.5f,0.5f,0.0f,1.0f
-    };
+        100.0f, 100.0f, 0.0f, 0.0f,
+        300.0f, 100.0f, 1.0f, 0.0f,
+        300.0f, 300.0f, 1.0f, 1.0f,
+        100.0f, 300.0f, 0.0f, 1.0f};
     unsigned int indices[] = {
         0, 1, 2,
         2, 3, 0};
 
     glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-    
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
-    //glBindVertexArray(VAO);
+    // glBindVertexArray(VAO);
 
     VertexBuffer vb(4 * 4 * sizeof(float), positions);
 
@@ -85,16 +84,21 @@ int main()
     vl.Push<float>(2);
     VertexArray va(VAO);
     va.Bind();
-    va.AddBuffer(vb,vl);
+    va.AddBuffer(vb, vl);
     // glEnableVertexAttribArray(0);
     // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-    //unsigned int ibo;
+    // unsigned int ibo;
     IndexBuffer indexBuffer(6 * sizeof(unsigned int), indices);
-    //glBindVertexArray(0);
+    // glBindVertexArray(0);
     va.UnBind();
 
-    glm::mat4 proj = glm::ortho(-2.0f,2.0f,-1.5f,1.5f,-1.0f,1.0f);
+    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+    // move camera 100 unit to right
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+    glm::mat4 mvp = proj * view * model;
 
     // ShaderProgram source = ParseShader("/Users/innovation/CodePlace/OpenGL_Example/src/resource/shader/Triangle.shader");
     // std::cout << "Vertex" << std::endl;
@@ -103,21 +107,21 @@ int main()
     // std::cout << source.fragment << std::endl;
     // unsigned int shader = CreateShader(source.vertex, source.fragment);
     Shader shader("/Users/innovation/CodePlace/OpenGL_Example/src/resource/shader/Triangle.shader");
-    int location = shader.GetLocationOfUniform("u_color");
+    int location = shader.GetLocationOfUniform("u_Color");
 
-    // int location = glGetUniformLocation(shader, "u_color");
+    // int location = glGetUniformLocation(shader, "u_Color");
     float r = 0.0f;
     float increment = 0.0f;
     shader.Bind();
-    shader.SetUniform4f("u_Color",0.8f,0.3f,0.8f,1.0f);
-    shader.SetUniformMat4f("u_MVP",proj);
+    shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+    shader.SetUniformMat4f("u_MVP", mvp);
     // glUseProgram(0);
 
     // start rendering
     Renderer renderer;
     Texture texture("/Users/innovation/CodePlace/OpenGL_Example/src/resource/texture/darksoul.png");
     texture.Bind();
-    shader.SetUniform1i("u_Texture",0);
+    shader.SetUniform1i("u_Texture", 0);
     while (glfwWindowShouldClose(window) == false)
     {
         // detect key escape event
@@ -128,14 +132,11 @@ int main()
         // clear color buffer
         glClear(GL_COLOR_BUFFER_BIT);
 
-        shader.SetUniform4f("u_Color",r, 0.8f, 0.5f, 1.0f);
+        shader.SetUniform4f("u_Color", r, 0.8f, 0.5f, 1.0f);
         // if (location != -1)
         // {
         //     glUniform4f(location, r, 0.8f, 0.5f, 1.0f);
         // }
-
-        
-
 
         if (r >= 1.0f)
         {
@@ -147,14 +148,14 @@ int main()
         }
         r += increment;
 
-        //glBindVertexArray(VAO);
-        // va.Bind();
-        // glUseProgram(shader);
-        // shader.Bind();
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-        // indexBuffer.Bind();
-        // glDrawArrays(GL_TRIANGLES,0,6);
-        renderer.Draw(va,indexBuffer,shader);
+        // glBindVertexArray(VAO);
+        //  va.Bind();
+        //  glUseProgram(shader);
+        //  shader.Bind();
+        //  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        //  indexBuffer.Bind();
+        //  glDrawArrays(GL_TRIANGLES,0,6);
+        renderer.Draw(va, indexBuffer, shader);
         // double buffer tech
         glfwSwapBuffers(window);
         // func call all the event since the last call
